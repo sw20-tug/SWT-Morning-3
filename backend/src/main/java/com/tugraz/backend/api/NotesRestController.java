3125c7760600568b3a6861d86b459e47b2f9e3b7
sharpyxi <anze.oster@gmail.com>
2020-03-25 11:31:16 +0100

    Revert "NOTE-2 post request handler [aoc,rm]"
    This reverts commit b85990fc

--- backend/src/main/java/com/tugraz/backend/api/NotesRestController.java
+++ backend/src/main/java/com/tugraz/backend/api/NotesRestController.java
@@ -2,8 +2,6 @@ package com.tugraz.backend.api;
 
 import com.tugraz.backend.models.Note;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -19,11 +17,5 @@ public class NotesRestController {
         mockNotesList.add(new Note("test2", "asdasdsadassda"));
         mockNotesList.add(new Note("test3", "ldfkldsflsdlfkds"));
         return mockNotesList;
-    }
-
-    @PostMapping("/notes")
-    public Note createNote(@RequestBody Note note) {
-        // TODO store into databse
-        return note;
     }
 }
