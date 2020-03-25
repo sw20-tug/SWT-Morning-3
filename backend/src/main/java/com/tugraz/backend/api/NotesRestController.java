0eb93431560ca111f950f332c3de24ec19d8e4d6
Robi <rm0595@student.uni-lj.si>
2020-03-25 11:02:06 +0100

    NOTE-18 mongodb connection and storage implementation [aoc,rm]

--- backend/src/main/java/com/tugraz/backend/api/NotesRestController.java
+++ backend/src/main/java/com/tugraz/backend/api/NotesRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -13,9 +14,13 @@ public class NotesRestController {
     @GetMapping("/notes")
     public List<Note> getAllNotes() {
         List<Note> mockNotesList = new ArrayList<>();
-        mockNotesList.add(new Note("test", "blablalblalba"));
-        mockNotesList.add(new Note("test2", "asdasdsadassda"));
-        mockNotesList.add(new Note("test3", "ldfkldsflsdlfkds"));
+        mockNotesList.add(new Note("test", "blablalblalba", new Date().getTime()));
+        mockNotesList.add(new Note("test2", "asdasdsadassda", new Date().getTime()));
+        mockNotesList.add(new Note("test3", "ldfkldsflsdlfkds", new Date().getTime()));
         return mockNotesList;
     }
+
+
+
+
 }
