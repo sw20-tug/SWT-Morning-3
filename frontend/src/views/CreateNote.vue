22e6574c6220c41c40bf78f8bbc4cac9e74228a9
Vlad Dumitru <dalv.urtimud@gmail.com>
2020-04-01 09:36:05 +0200

    NOTE-14 Set up VueRouter [DV, JL, TA]

---
+++ frontend/src/views/CreateNote.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="note-container">
+    <NewNote />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import NewNote from '@/components/NewNote.vue'
+
+@Component({
+  components: {
+    NewNote
+  }
+})
+export default class App extends Vue {
+  mounted () {
+    this.$store.dispatch('sync')
+  }
+}
+</script>
+
+<style lang="scss">
+.note-container {
+  display: flex;
+  flex-direction: column;
+  max-width: 40rem;
+}
+</style>
