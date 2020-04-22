5de9e69a7e6eae0252bf97b45beb2d494fb3bf11
Ledio Jahaj <ljahaj@student.tugraz.at>
2020-04-22 09:40:39 +0200

    Bug

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -3,21 +3,14 @@ import Router from 'vue-router'
 
 import Overview from './views/Overview.vue'
 import CreateNote from './views/CreateNote.vue'
-<<<<<<< HEAD
-import EditNote from "@/views/EditNote.vue";
-=======
->>>>>>> upstream/develop
+import EditNote from "@/views/EditNote.vue"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path: '/', component: Overview },
-<<<<<<< HEAD
     { path: '/new', component: CreateNote },
     { path: '/edit/:id', component: EditNote}
-=======
-    { path: '/new', component: CreateNote }
->>>>>>> upstream/develop
   ]
 })
