c1f1f27620e8439b4fe6bb086f262e75ccfc703e
Andre Tropper <andre.tropper@avl.com>
2020-04-22 12:05:04 +0200

    feature_NOTE_003_integration_tests

--- frontend/tests/e2e/specs/new_note_spec.js
+++ frontend/tests/e2e/specs/new_note_spec.js
@@ -1,16 +1,12 @@
 describe('New note page', () => {
-  before(() =>
-    cy.exec('echo "{ notes: [] }" > data-mock.json')
-  )
-
   it('successfully loads', () => {
     cy.visit('/#/new')
   })
 
   it('shows post title input', () => {
     cy.visit('/#/new')
-    
-    cy.get('input').then(el => 
+
+    cy.get('input').then(el =>
       expect(el).to.exist
     )
   })
