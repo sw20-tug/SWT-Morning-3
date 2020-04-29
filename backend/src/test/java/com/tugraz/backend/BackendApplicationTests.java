c48478ab46b8bff56e136879caf0c8e107261103
Robi <rm0595@student.uni-lj.si>
2020-04-29 14:58:13 +0200

    NOTE-2 create/delete test [aoc,rm]

--- backend/src/test/java/com/tugraz/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/tugraz/backend/BackendApplicationTests.java
@@ -1,13 +1,32 @@
 package com.tugraz.backend;
 
+import com.tugraz.backend.api.NotesRestController;
+import com.tugraz.backend.db.NotesDatabase;
+import com.tugraz.backend.models.Note;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class BackendApplicationTests {
 
+    @Autowired
+    private NotesDatabase notesDb;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void createDeleteNote() {
+        Note testNote = new Note();
+        testNote.setId("test");
+        assert "test".equals(notesDb.save(testNote).getId());
+
+        notesDb.deleteById("test");
+        assert !notesDb.findById("test").isPresent();
+
     }
 
 }
