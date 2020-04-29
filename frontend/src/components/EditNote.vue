f384d322e1947912bd69378f6a9e18b4297fffa4
Andre Tropper <andre.tropper@avl.com>
2020-04-29 12:06:36 +0200

    feature-NOTE_006 Pin note #6

--- frontend/src/components/EditNote.vue
+++ frontend/src/components/EditNote.vue
@@ -23,15 +23,19 @@ export default class Note extends Vue {
   @Prop({ default: 0 }) readonly id!: string
   private title = ''
   private description = ''
+  private pinned = false
+  private tags: string[] = []
 
   async mounted () {
     const note: NoteData = await service.getNoteById(this.id)
     this.title = note.title
     this.description = note.description
+    this.pinned = note.pinned
+    this.tags = note.tags
   }
 
   editNote () {
-    service.editNote(this.id, this.title, this.description)
+    service.editNote(this.id, this.title, this.description, this.pinned, this.tags)
       .then(() => this.$router.go(-1))
   }
 }
--- frontend/src/components/Note.vue
+++ frontend/src/components/Note.vue
@@ -3,6 +3,7 @@
     <div class="note-header">
       <p class="note-title">{{ title }}<span hidden>{{id}}</span></p>
       <p class="note-date">{{ date }}</p>
+      <p class="note-pin"><a v-if="pinned == null || pinned == false" href="#" @click="pinNote(id, title, description, true, tags)">pin</a><a v-if="pinned == true" href="#" @click="pinNote(id, title, description, false, tags)">unpin</a></p>
     </div>
     <div class="note-body">
       <p>{{ description }}</p>
@@ -26,17 +27,25 @@ const service = new NoteService()
 
 @Component({})
 export default class Note extends Vue {
-  @Prop({ default: 0 }) readonly id!: number
+  @Prop({ default: 0 }) readonly id!: string
   @Prop({ default: '' }) readonly title!: string
   @Prop({ default: '' }) readonly description!: string
   @Prop({ default: new Date(0) }) readonly timestamp!: Date
+  @Prop({ default: false }) readonly pinned!: boolean
+  @Prop({ default: [] }) readonly tags!: string[]
 
   get date () {
+    console.log(this.timestamp)
     return moment(this.timestamp).format('YYYY-MM-DD')
   }
 
   deleteNote (id: string) {
     service.deleteNote(id)
+      .then(() => this.$store.dispatch('sync'))
+  }
+
+  pinNote (id: string, title: string, description: string, pin: boolean, tags: string []) {
+    service.editNote(id, title, description, pin, tags)
       .then(() => this.$store.dispatch('sync'))
   }
 }
@@ -63,6 +72,10 @@ export default class Note extends Vue {
     }
 
     & > p.note-date {
+      text-align: right;
+    }
+
+    & > p.note-pin {
       text-align: right;
     }
   }
